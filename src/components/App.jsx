faf10c1a5074358bee2b4867eee359f94a2526d5
Facundo Carrion <facundojcarrion@gmail.com>
2022-12-12 09:07:27 -0300

    ghpages deploy

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,13 +5,18 @@ import Button from './Button'
 
 function App() {
   const [count, setCount] = useState(0)
-  const sumar = () => setCount(count+1)
-  const restar = () => setCount(count-1)
+  const [amount, setAmount] = useState(1)
+  const [active, setActive] = useState(1)
+
+  const sumar = () => setCount(count + amount)
+  const restar = () => setCount(count - amount)
   const restart = () => setCount(0)
+  const isActive = (num) => active === num ? 'active' : "";
+
   return (
     <div className='container'>
       <div className='container__count'>{count}</div>
-      <Button sumar={sumar} restart={restart} restar={restar}/>
+      <Button sumar={sumar} restart={restart} restar={restar} setAmount={setAmount} setActive={setActive} active={active} isActive={isActive} />
     </div>
   )
 }
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,30 @@
 import '../assets/styles/App.css'
 
-function Button ( {sumar, restar, restart} ) {
+function Button ( {sumar, restar, restart, setAmount, setActive, active, isActive} ) {
   return (
     <div className='container__pannel'>
+      <div className='container__subpannel'>
+        <button 
+          className={isActive(1)} 
+          onClick={() => {setAmount(1); setActive(1)}}>
+          1
+        </button>
+        <button 
+          className={isActive(2)} 
+          onClick={() => {setAmount(2); setActive(2)}}>
+          2
+        </button>
+        <button
+          className={isActive(3)} 
+          onClick={() => {setAmount(5); setActive(3)}}>
+          5
+        </button>
+        <button
+          className={isActive(4)}  
+          onClick={() => {setAmount(10); setActive(4)}}>
+          10
+        </button>
+      </div>
       <button className='container__button click' onClick={sumar}>
         Click
       </button>
