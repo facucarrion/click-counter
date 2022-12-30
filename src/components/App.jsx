17af395370822d57cd7f8fdb90f6f32e9c16cf7a
Facundo Carrion <facundojcarrion@gmail.com>
2022-12-30 18:54:55 -0300

    feature(all): all finished

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,36 @@
-import { useState } from 'react'
-import '/src/assets/styles/App.css'
-import Button from './Button'
-import {Title, Watermark} from './Marks'
-
+import { useState } from "react";
+import "/src/assets/styles/App.css";
+import Button from "./Button";
+import { Title, Watermark } from "./Marks";
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [amount, setAmount] = useState(1)
-  const [active, setActive] = useState(1)
+  const [count, setCount] = useState(0);
+  const [amount, setAmount] = useState(1);
+  const [active, setActive] = useState(1);
 
-  const sumar = () => setCount(count + amount)
-  const restar = () => setCount(count - amount)
-  const restart = () => setCount(0)
-  const isActive = (num) => active === num ? 'active' : "";
+  const sumar = () => setCount(count + amount);
+  const restar = () => setCount(count - amount);
+  const restart = () => setCount(0);
+  const isActive = (num) => (active === num ? "active" : "");
 
   return (
     <>
-    <Title />
-    <div className='container'>
-      <div className='container__count'>{count}</div>
-      <Button sumar={sumar} restart={restart} restar={restar} setAmount={setAmount} setActive={setActive} active={active} isActive={isActive} />
-    </div>
-    <Watermark />
+      <Title />
+      <div className="container">
+        <div className="container__count">{count}</div>
+        <Button
+          sumar={sumar}
+          restart={restart}
+          restar={restar}
+          setAmount={setAmount}
+          setActive={setActive}
+          active={active}
+          isActive={isActive}
+        />
+      </div>
+      <Watermark />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,41 +1,57 @@
-import '../assets/styles/App.css'
+import "../assets/styles/App.css";
 
-function Button ( {sumar, restar, restart, setAmount, setActive, isActive} ) {
+function Button({ sumar, restar, restart, setAmount, setActive, isActive }) {
   return (
-    <div className='container__pannel'>
-      <div className='container__subpannel'>
-        <button 
-          className={isActive(1)} 
-          onClick={() => {setAmount(1); setActive(1)}}>
+    <div className="container__pannel">
+      <div className="container__subpannel">
+        <button
+          className={isActive(1)}
+          onClick={() => {
+            setAmount(1);
+            setActive(1);
+          }}
+        >
           1
         </button>
-        <button 
-          className={isActive(2)} 
-          onClick={() => {setAmount(2); setActive(2)}}>
+        <button
+          className={isActive(2)}
+          onClick={() => {
+            setAmount(2);
+            setActive(2);
+          }}
+        >
           2
         </button>
         <button
-          className={isActive(3)} 
-          onClick={() => {setAmount(5); setActive(3)}}>
+          className={isActive(3)}
+          onClick={() => {
+            setAmount(5);
+            setActive(3);
+          }}
+        >
           5
         </button>
         <button
-          className={isActive(4)}  
-          onClick={() => {setAmount(10); setActive(4)}}>
+          className={isActive(4)}
+          onClick={() => {
+            setAmount(10);
+            setActive(4);
+          }}
+        >
           10
         </button>
       </div>
-      <button className='container__button click' onClick={sumar}>
+      <button className="container__button click" onClick={sumar}>
         Click
       </button>
-      <button className='container__button restar' onClick={restar}>
+      <button className="container__button restar" onClick={restar}>
         Substract
       </button>
-      <button className='container__button restart' onClick={restart}>
+      <button className="container__button restart" onClick={restart}>
         Restart
       </button>
     </div>
-  )
+  );
 }
 
-export default Button
+export default Button;
